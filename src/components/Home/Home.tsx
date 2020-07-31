70981ed322a7263c2bff73e45a4870848f784ff6
bradyk <karenbrady@rentalcars.com>
2020-07-31 12:19:59 +0200

    added react-spring to InfoSection images

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -14,19 +14,22 @@ export const Home = () => {
         path="/map"
         title="Map"
         paragraph="Use the map to build up a record of places you have been and places you wish to go to"
+        background="https://images.unsplash.com/photo-1513492365349-8ba97c199501?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
       />
       <InfoSection
         path="/destinations"
         title="Destinations"
         paragraph="Check out destinations of the world and shared itineraries from other users"
+        background="https://images.unsplash.com/photo-1513492365349-8ba97c199501?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
       />
       <InfoSection
         path="/itineraries"
         title="Itineraries"
         paragraph="Plan your trip and add your own custom itineraries"
+        background="https://images.unsplash.com/photo-1513492365349-8ba97c199501?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
       />
       <Newsletter />
-      <ScrollArrow/>
+      <ScrollArrow />
     </>
   );
 };
--- src/components/InfoSection/InfoSection.tsx
+++ src/components/InfoSection/InfoSection.tsx
@@ -1,19 +1,35 @@
 import React, { FC } from 'react';
 import { Container } from './InfoSection.styles';
 import { Link } from 'react-router-dom';
-import { Image } from 'semantic-ui-react';
+// import { Image } from 'semantic-ui-react';
+import { useSpring, animated } from 'react-spring';
+import './infoSection.css';
 
 interface InfoSectionProps {
   title: string;
   paragraph: string;
   path: string;
+  background: string;
 }
+
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 20,
+  (x - window.innerWidth / 2) / 20,
+  1.1,
+];
+const trans = (x, y, s) => `perspective(600px)  scale(${s})`;
 
 export const InfoSection: FC<InfoSectionProps> = ({
   title,
   paragraph,
   path,
+  background,
 }) => {
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 0.7, tension: 350, friction: 40 },
+  }));
+
   return (
     <Container>
       <h1>{title}</h1>
@@ -21,17 +37,16 @@ export const InfoSection: FC<InfoSectionProps> = ({
       <Link to={path}>
         <button>Click here</button>
       </Link>
-      {/* <ImageContainer> */}
-      <Image
-        src={
-          'https://images.unsplash.com/photo-1513492365349-8ba97c199501?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
-        }
-        style={{ height: 100, width: 100 }}
-        alt="Map"
-        size="medium"
-        floated="right"
+      <animated.div
+        className="card"
+        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+        onMouseLeave={() => set({ xys: [0, 0, 1] })}
+        style={{
+          // @ts-ignore
+          transform: props.xys.interpolate(trans),
+          backgroundImage: `url(${background})`,
+        }}
       />
-      {/* </ImageContainer> */}
     </Container>
   );
 };
