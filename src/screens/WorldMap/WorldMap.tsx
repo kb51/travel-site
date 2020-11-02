fb8d366e6015ff48518ecbb6e96dd7d987ac42bb
kb51 <karenbrady92@hotmail.co.uk>
2020-11-02 13:44:18 +0000

    add Footer to WorldMap

--- src/screens/WorldMap/WorldMap.tsx
+++ src/screens/WorldMap/WorldMap.tsx
@@ -17,6 +17,7 @@ import {
   FullscreenControlWrapper,
   ScaleControlWrapper,
 } from './Map.styles';
+import { Footer } from '../../components/Footer';
 
 export function WorldMap() {
   const [viewport, setViewport] = useState({
@@ -40,57 +41,60 @@ export function WorldMap() {
   const { bucketlistCountries, setBucketlistCountries } = bucketlist;
 
   return (
-    <Container>
-      <Title>Map</Title>
-      <AddDestination />
-      <MapGL
-        {...viewport}
-        mapStyle="mapbox://styles/kb51/ckawuxjic00to1ipp60a37454"
-        onViewportChange={(viewport) => {
-          // @ts-ignore
-          setViewport(viewport);
-        }}
-        mapboxApiAccessToken={process.env.REACT_APP_MAP_TOKEN}
-      >
-        <Pins
-          color="limegreen"
-          data={visitedCountries}
-          onClick={_onClickMarker}
-        />
-        <Pins
-          color="dodgerblue"
-          data={bucketlistCountries}
-          onClick={_onClickMarker}
-        />
-        {popupInfo ? (
-          <Popup
-            tipSize={5}
-            anchor="top"
-            longitude={popupInfo.country.latlng[1]}
-            latitude={popupInfo.country.latlng[0]}
-            closeOnClick={false}
-            onClose={() => setPopUpInfo(null)}
-          >
-            <PinInfo
-              setPopUpInfo={setPopUpInfo}
-              visitedCountries={visitedCountries}
-              setVisitedCountries={setVisitedCountries}
-              bucketlistCountries={bucketlistCountries}
-              setBucketlistCountries={setBucketlistCountries}
-              info={popupInfo}
-            />
-          </Popup>
-        ) : null}
-        <FullscreenControlWrapper>
-          <FullscreenControl />
-        </FullscreenControlWrapper>
-        <NavWrapper>
-          <NavigationControl />
-        </NavWrapper>
-        <ScaleControlWrapper>
-          <ScaleControl />
-        </ScaleControlWrapper>
-      </MapGL>
-    </Container>
+    <>
+      <Container>
+        <Title>Map</Title>
+        <AddDestination />
+        <MapGL
+          {...viewport}
+          mapStyle="mapbox://styles/kb51/ckawuxjic00to1ipp60a37454"
+          onViewportChange={(viewport) => {
+            // @ts-ignore
+            setViewport(viewport);
+          }}
+          mapboxApiAccessToken={process.env.REACT_APP_MAP_TOKEN}
+        >
+          <Pins
+            color="limegreen"
+            data={visitedCountries}
+            onClick={_onClickMarker}
+          />
+          <Pins
+            color="dodgerblue"
+            data={bucketlistCountries}
+            onClick={_onClickMarker}
+          />
+          {popupInfo ? (
+            <Popup
+              tipSize={5}
+              anchor="top"
+              longitude={popupInfo.country.latlng[1]}
+              latitude={popupInfo.country.latlng[0]}
+              closeOnClick={false}
+              onClose={() => setPopUpInfo(null)}
+            >
+              <PinInfo
+                setPopUpInfo={setPopUpInfo}
+                visitedCountries={visitedCountries}
+                setVisitedCountries={setVisitedCountries}
+                bucketlistCountries={bucketlistCountries}
+                setBucketlistCountries={setBucketlistCountries}
+                info={popupInfo}
+              />
+            </Popup>
+          ) : null}
+          <FullscreenControlWrapper>
+            <FullscreenControl />
+          </FullscreenControlWrapper>
+          <NavWrapper>
+            <NavigationControl />
+          </NavWrapper>
+          <ScaleControlWrapper>
+            <ScaleControl />
+          </ScaleControlWrapper>
+        </MapGL>
+      </Container>
+      <Footer />
+    </>
   );
 }
